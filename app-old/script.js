65b89816834bdb9d280c568e15c34357f962c492
Allard Schuurmans <allard.schuurmans@gmail.com>
2020-02-01 15:20:26 +0100

    adds directives

--- app-old/script.js
+++ app-old/script.js
@@ -1,13 +1,35 @@
-angular.module('AngularJsExample', [])
+angular.module('AngularJsExample', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "./home.html",
+                controller: "homeController"
+            })
+            .when("/Got", {
+                templateUrl: "./gotTable.html",
+                controller: "gotController"
+            })
+    })
+    .controller('navController', ['$scope', function ($scope) {
+        $scope.searchLabel = 'Search';
+
+    }])
     .controller('homeController', ['$scope', function ($scope) {
+        $scope.currentMenuItem = "home";
         $scope.title = "This is the home page";
 
-        
+
     }])
-    .controller('gotController', ['$scope', 'GoTService', function($scope, GoTService){
-        
-        GoTService.getCharacters(1, 100).then(function (result) {
+    .controller('gotController', ['$scope', 'GoTService', function ($scope, GoTService) {
+        $scope.currentMenuItem = 'got';
+        GoTService.getCharacters(1, 20).then(function (result) {
             $scope.characters = result.data;
 
         });
     }])
+    .directive('bread', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'breadcrumb.html'
+        };
+    })
