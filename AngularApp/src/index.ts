76a13868abf271f99df2c1a9f558bf1e55c8b615
Allard Schuurmans <allard.schuurmans@gmail.com>
2020-02-16 21:20:09 +0100

    cleaned up web.config

--- AngularApp/src/index.ts
+++ AngularApp/src/index.ts
@@ -1,6 +1,10 @@
-import './app-old/libs/breadcrumb';
+
+import 'angular';
+import 'angular-ui-router';
+import './app-old/libs/breadcrumb/angular-breadcrumb.min';
+import './app-old/script';
 import './app-old/services/gotService';
 import './app-old/services/navService';
-import './app-old/script';
+
 
 
--- AngularApp/webpack.config.js
+++ AngularApp/webpack.config.js
@@ -1,6 +1,39 @@
 var path = require('path');
-var webpack = require('webpack');
-var helpers = require('../config/helpers');
-
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = {
+    mode: 'development',
+    entry: {
+        'ng1': './src/index.ts'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/dev'),
+        filename: '[name].bundle.js'
+    },
+
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader', 'angular2-template-loader?keepUrl=true'],
+                exclude: [/\.(spec|e2e)\.ts$/]
+            },
+            {
+              test: /\.css$/,
+              loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: 'config/index.html'
+        })
+    ]
+}
